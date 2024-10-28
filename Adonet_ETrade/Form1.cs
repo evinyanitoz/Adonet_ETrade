d3a76cdf833277b7fc5f136869bf20c2e7cc85c3
evinyanitoz <eyanitoz@gmail.com>
2024-10-29 00:41:51 +0300

    Add project files.

---
+++ Adonet_ETrade/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adonet_ETrade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ProductDal product= new ProductDal();
+            dgwProducts.DataSource = product.GetAll();
+        }
+    }
+}
---
+++ Adonet_ETrade/ProductDal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adonet_ETrade
+{
+    public class ProductDal
+    {
+
+        public DataTable GetAll()
+        {
+            SqlConnection baglan = new SqlConnection(@"Server=LAPTOP-F5PIAQHS\BOTANIKSQL;Initial Catalog=ETrade;integrated security=true");
+           
+            if (baglan.State ==ConnectionState.Closed)
+            {
+                
+                baglan.Open();
+            }
+            SqlCommand komut = new SqlCommand("select * from Products",baglan);
+            //Listelediğimizi nereye gönderecez komutu bağlana gönder.
+            //komutu yazdık şimdi EXECUTE Edeceğiz.
+
+            SqlDataReader oku = komut.ExecuteReader(); 
+            DataTable dt=new DataTable();
+            dt.Load(oku);
+            oku.Close();
+            baglan.Close();
+            return dt;
+
+                    
+        }
+
+    }
+}
